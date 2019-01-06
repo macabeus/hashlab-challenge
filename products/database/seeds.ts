f6ad2820158c5a03fa49c1bd341fccde7771b053
macabeus <bruno.macabeus@gmail.com>
2019-01-06 02:59:44 -0200

    database: add Users table

--- products/database/seeds.ts
+++ products/database/seeds.ts
@@ -38,6 +38,40 @@ const createProductsTable = async () => {
   })
 }
 
+const createUsersTable = async () => {
+  console.log('Creating users table...')
+
+  const params: AWS.DynamoDB.CreateTableInput = {
+    TableName: 'Users',
+    KeySchema: [
+      {
+        AttributeName: 'Id',
+        KeyType: 'HASH',
+      },
+    ],
+    AttributeDefinitions: [
+      {
+        AttributeName: 'Id',
+        AttributeType: 'N',
+      },
+    ],
+    ProvisionedThroughput: {
+      ReadCapacityUnits: 1,
+      WriteCapacityUnits: 1,
+    },
+  }
+
+  return new Promise((resolve, reject) => {
+    dynamo.createTable(params, (error, data) => {
+      if (error) {
+        return reject(error)
+      }
+
+      resolve(data)
+    })
+  })
+}
+
 const populateProducts = async () => {
   console.log('Populating products...')
 
@@ -89,8 +123,61 @@ const populateProducts = async () => {
   })
 }
 
+const populateUsers = async () => {
+  console.log('Populating users...')
+
+  const params: AWS.DynamoDB.BatchWriteItemInput = {
+    RequestItems: {
+      Users: [
+        {
+          PutRequest: {
+            Item: {
+              Id: { N: '1' },
+              FirstName: { S: 'Macabeus' },
+              LastName: { S: 'Aquino' },
+              DateOfBirth: { S: '1996-06-01' },
+            },
+          },
+        },
+        {
+          PutRequest: {
+            Item: {
+              Id: { N: '2' },
+              FirstName: { S: 'Marco' },
+              LastName: { S: 'Worms' },
+              DateOfBirth: { S: '1993-08-04' },
+            },
+          },
+        },
+        {
+          PutRequest: {
+            Item: {
+              Id: { N: '3' },
+              FirstName: { S: 'Mateus' },
+              LastName: { S: 'Moog' },
+              DateOfBirth: { S: '1991-01-14' },
+            },
+          },
+        },
+      ],
+    },
+  }
+
+  return new Promise((resolve, reject) => {
+    dynamo.batchWriteItem(params, (error, data) => {
+      if (error) {
+        return reject(error)
+      }
+
+      resolve(data)
+    })
+  })
+}
+
 createProductsTable()
+  .then(() => createUsersTable())
   .then(() => populateProducts())
+  .then(() => populateUsers())
   .catch(e => {
     console.log('Fatal Error!!')
     console.log(e)
