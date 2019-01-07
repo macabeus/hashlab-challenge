700c67c4f3f386acb392d9c8bbd2ce913b18b5b6
macabeus <bruno.macabeus@gmail.com>
2019-01-07 11:52:17 -0200

    grpc: add client grpc logic at Product service to get the discount

--- products/routes/products.ts
+++ products/routes/products.ts
@@ -1,8 +1,24 @@
 import * as KoaRouter from 'koa-router'
 import { index } from '../database/products'
+import { getDiscount } from '../grpc_client/discount_service'
 
 const productsRoute = (router: KoaRouter) => router.get('/product', async ctx => {
-  ctx.body = await index(ctx.dynamo)
+  const products = await index(ctx.dynamo)
+
+  const productsDiscount = await Promise.all(products.map(async product => {
+    try {
+      const discount = await getDiscount(ctx.grpcClient, product.id, 1)
+
+      product.discount.pct = discount
+      product.discount.value_in_cents = product.price_in_cents * discount / 100
+
+      return product
+    } catch (e) {
+      return product
+    }
+  }))
+
+  ctx.body = productsDiscount
 })
 
 export default productsRoute
