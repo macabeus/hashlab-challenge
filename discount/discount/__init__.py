11214ed5f4b83999d67b828a4ca56a41369394d5
macabeus <bruno.macabeus@gmail.com>
2019-01-08 01:12:25 -0200

    discount: add intermittent error simulation

--- discount/discount/__init__.py
+++ discount/discount/__init__.py
@@ -2,6 +2,7 @@ from typing import Optional
 from datetime import datetime
 from .birthday import birthday
 from .blackfriday import blackfriday
+from .intermittent import intermittent
 from models.user import User
 
 
@@ -13,7 +14,14 @@ def _limit(discount: int):
     return discount
 
 
-def apply(current_date: datetime, user: Optional[User]) -> int:
+def apply(
+    current_date: datetime,
+    user: Optional[User],
+    product_id: int,
+    intermittent_flag: bool
+) -> int:
+    intermittent(intermittent_flag, product_id)
+
     gross_discount = 0
 
     gross_discount += blackfriday(current_date)
